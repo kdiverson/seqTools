afe3bbea599d15ef2e4269b6a99d2f22b25f6bb6
Kathryn Iverson <kd.iverson@gmail.com>
2011-06-13 16:21:39 -0400

    added calcN50

---
+++ calcN50.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+"""Usage: python calcN50.py /path/to/contigFile.fasta"""
+import sys
+
+infile = open(sys.argv[1], 'r')
+
+def get_next_fasta (fileObject):
+    '''usage: for header, seq in get_next_fasta(fileObject):
+    '''
+    header = ''
+    seq = ''
+    for line in fileObject:
+        if line.startswith('>'):
+            header = line.strip()
+            break
+    
+    for line in fileObject:
+        if line.startswith('>'):
+            yield header, seq
+            header = line.strip()
+            seq = ''
+        else:
+            seq += line.strip()
+    #yield the last entry
+    if header:
+        yield header, seq
+
+contigs = []
+total = 0
+
+for header, seq in get_next_fasta(infile):
+   length = len(seq) 
+   contigs.append(length)
+   total += length
+
+halfTot = 0
+contigs.sort()
+
+for contig in contigs:
+    halfTot += contig
+    if halfTot>total/2:
+        print "N50:", contig
+        break
